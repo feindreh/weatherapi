691492c050b23015c87ff2f216aaa08a11d6b397
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-05 01:59:57 +0200

    autoComplete filter out undefined city names

--- src/components/search.js
+++ src/components/search.js
@@ -18,14 +18,10 @@ function Search(){
     }
     async function autoComplete(input = input){
         //autocomplete input string
-
-
         if(input.length < 3){
             //api doesnt support strings with less then 3 letters
             return
         }
-
-
         //fetch data
         const data = await getAutoData(input)
         const arr = []
@@ -33,13 +29,12 @@ function Search(){
             let prop = d.properties
             arr.push(prop)
         })
-
         //get city names without duplicates
         const cityNameArray = arr.map((ob) => ob.city)
         const singleValues = new Set(cityNameArray)
-
         //setState to get the rerender 
-        setAuto([...singleValues])
+        const returnValues = [...singleValues].filter((a) => { return a!==undefined})
+        setAuto([...returnValues])
     }
     async function getLocations(){
         //use weather API with input value to find possible Locations
@@ -52,8 +47,7 @@ function Search(){
     return (
         <div>
             <input ref={inputRef} onChange = {handleChange}  type="text"  placeholder="City?"></input>
-            <button type ="button" onClick = {autoComplete}>AutoCompelte</button>
-            <button type="button" onClick={getLocations}>Gib Mir Wätta</button>
+            <button type="button" onClick={getLocations}>Wetter rausfinden</button>
             <div>
                 {auto.map((name) => {return (<Autocomplete key={name} name = {name} cb={()=>{autoSelected(name)}}/>)})}
             </div>
