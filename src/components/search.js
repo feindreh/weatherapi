55911f0432e4b08ed070d3333709771e878ad48c
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-04 07:32:41 +0200

    add autocomplete function and display results

--- src/components/search.js
+++ src/components/search.js
@@ -1,30 +1,43 @@
 import { useState} from "react";
 
 import getCityData from "../API/Weather"
+import getAutoData from "../API/autoComplete"
+
+import Autocomplete from "./autocomplete";
 
 function Search(){
 
     const [input,setInput] = useState("")
+    const [auto,setAuto] = useState([])
 
-
-    async function  handleChange(e){
+    async function handleChange(e){
         //handle State
         setInput(e.target.value)
-        //google maps API => auto complete
-    }
-
-    async function handleClick(){
-        //use weather API with input value to find possible Locations
-        console.log("Use Weather API with",input)
-        const possibleLocations = await getCityData(input)
-        possibleLocations.forEach((loc) => {console.log(loc)})
+        //auto complete
         
+    }
+    async function autoComplete(e){
+        //autocomplete input string
+        const data = await getAutoData(input)
+        const arr = []
+        data.features.forEach((d) => {
+            let prop = d.properties
+            arr.push(prop)
+        })
+        setAuto(arr)
+    }
+    async function getLocations(e){
+        //use weather API with input value to find possible Locations
     }
 
     return (
         <div>
             <input onChange = {handleChange}  type="text"  placeholder="City?"></input>
-            <button type="button" onClick={handleClick}>Gib Mir Wätta</button>
+            <button type ="button" onClick = {autoComplete}>AutoCompelte</button>
+            <button type="button" onClick={getLocations}>Gib Mir Wätta</button>
+            <div>
+                {auto.map((prop) => {return (<Autocomplete key={prop.place_id}city = {prop}/>)})}
+            </div>
         </div>
     )
 }
