6866b022e01090a1bbea26f86dbd2c2047cf4199
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-04 05:06:26 +0200

    add Search and Content components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 
 
+import Content from "./components/content";
+import Search from "./components/search";
 function App() {
+
+
+
+
   return (
     <div>
+      <Search/>
+      <Content/>
     </div>
   );
 }
