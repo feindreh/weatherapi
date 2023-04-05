c56a3e07e1342882a538c840e60f9e645a1d5d0c
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-05 04:54:40 +0200

    connect weather api and store it as state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-
+import { useState,useEffect } from "react";
 
 import Content from "./components/content";
 import Search from "./components/search";
+
+import getWeather from "./API/Weather";
+
 function App() {
 
+  const [data,setData] = useState([])
 
+  useEffect(()=>{
+   startSearch("Heidelberg")
+},[])
+
+  async function startSearch(cityName){
+    //cb for search component
+    
+    //start the whole search api thing
+    const data = await getWeather(cityName)
+    setData(data)
+  }
 
 
   return (
     <div>
-      <Search/>
-      <Content/>
+      <Search search={startSearch}/>
+      <Content data={data}/>
     </div>
   );
 }
