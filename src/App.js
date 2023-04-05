0785d3f9418c489b54e41dfbc41d5af417de3448
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-05 07:13:57 +0200

    nest search into content

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import "./style/App.css"
+
 import { useState,useEffect } from "react";
 
 import Content from "./components/content";
-import Search from "./components/search";
 
 import getWeather from "./API/Weather";
 
+
+
 function App() {
 
-  const [data,setData] = useState([])
+  const [data,setData] = useState({})
 
   useEffect(()=>{
    startSearch("Heidelberg")
 },[])
 
-  async function startSearch(cityName){
+  async function startSearch(cityName,cb){
+    setData({})
     //cb for search component
     
     //start the whole search api thing
@@ -21,11 +25,10 @@ function App() {
     setData(data)
   }
 
-
+  console.log(data)
   return (
-    <div>
-      <Search search={startSearch}/>
-      <Content data={data}/>
+    <div id="App">
+      <Content data={data} search={startSearch}/>
     </div>
   );
 }
