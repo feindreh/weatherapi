6501b713cb2b9282c9b87f3de85872a9d9357af3
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-05 04:19:33 +0200

    add callback from App component to button click

--- src/components/search.js
+++ src/components/search.js
@@ -1,29 +1,27 @@
 import { useState,useRef,useEffect} from "react";
 
-import getCityData from "../API/Weather"
 import getAutoData from "../API/autoComplete"
 
 import Autocomplete from "./autocomplete";
 
-function Search(){
+function Search(props){
+
+    const {search} = props
 
     const inputRef = useRef()
     const [auto,setAuto] = useState([])
     const [timer,setTimer] = useState(null)
-
-    useEffect(() => {
-        //clear timer on unmount
-        return () => clearTimeout(timer);
-      }, [timer]);
     
-    async function handleChange(e){
-        //clear auto then reset a timer and start it to find new autocompletes
-        // ==> we will only get autocompletes if we stop typing for some time
-        // ==> saves alot of api calls
-
-        //setAuto([])
-        AutoCompelteTimer(e.target.value)
+    function handleButton(){
+         //clear Autocomplets + Timer
+         // call callback search from props with input value
+        clearTimeout(timer);
+        let input = inputRef.current.value
         
+        inputRef.current.value = ""
+        setAuto([])
+
+        search(input)
     }
     function AutoCompelteTimer(string){
         //clear timer and start it again to get autocompletes after some time
@@ -33,7 +31,7 @@ function Search(){
         },250)) 
     }
     async function autoComplete(input){
-        //autocomplete input string
+        //autocomplete input string with api
         if(input.length < 3){
             //api doesnt support strings with less then 3 letters
             return
@@ -46,25 +44,22 @@ function Search(){
             arr.push(prop)
         })
         //get city names without duplicates
-        console.log(arr)
         const cityNameArray = arr.map((ob) => ob.city)
         const singleValues = new Set(cityNameArray)
         //setState to get the rerender 
         const returnValues = [...singleValues].filter((a) => { return a!==undefined})
         setAuto([...returnValues])
     }
-    async function getLocations(){
-        //use weather API with input value to find possible Locations
-    }
     function autoSelected(cityName){
+        //callback for Autocomplete component
         inputRef.current.value = cityName
         setAuto([])
     }
 
     return (
         <div>
-            <input ref={inputRef} onChange = {handleChange}  type="text"  placeholder="City?"></input>
-            <button type="button" onClick={getLocations}>Wetter rausfinden</button>
+            <input ref={inputRef} onChange = {(e)=>{ AutoCompelteTimer(e.target.value)}}  type="text"  placeholder="City?"></input>
+            <button type="button" onClick={handleButton}>Wetter rausfinden</button>
             <div>
                 {auto.map((name) => {return (<Autocomplete key={name} name = {name} cb={()=>{autoSelected(name)}}/>)})}
             </div>
