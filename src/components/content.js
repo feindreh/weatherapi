25bc1733d7cc9a6bfb974f2e18dc1a973ee1bd37
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-05 07:14:04 +0200

    initial

--- src/components/content.js
+++ src/components/content.js
@@ -1,6 +1,35 @@
-function Content(){
+import "../style/Content.css"
+
+import {KelvinToCelsius,msToKmh} from "../utility"
+
+import Search from "./search"
+
+function Content(props){
+   const {search,data} = props 
+   const{name,sys,main,weather,wind} = data
+
+    if(name === undefined){
+        return(
+            <>Loading ....</>
+        )
+    }
     return (
-        <>Content</>
+        <div id="content">
+            <div className="flexContainer">
+                <div>{weather[0].description}</div>
+                <div>{name} {sys.country}</div>
+                <div>Datum ?</div>
+                <div>{KelvinToCelsius(main.temp)} °C</div>
+                <div>{weather[0].icon}</div>
+                <Search search={search}/>
+            </div>
+            <div className="flexContainer">
+                <div>Feelslike: {KelvinToCelsius(main.feels_like)} °C</div>
+                <div>Humidity: {main.humidity} %</div>
+                <div>Windspeed: {msToKmh(wind.speed)}/kmh</div>
+                <div>Direction: {wind.deg}</div>
+            </div>
+        </div>
     )
 }
 
