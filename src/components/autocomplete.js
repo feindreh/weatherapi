6a8aa4bf0e9a2638870f98d74227dc07a9d0b0fc
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-06 03:32:18 +0200

    add icon

--- src/components/autocomplete.js
+++ src/components/autocomplete.js
@@ -1,9 +1,11 @@
-import "../style/autocomplete.css"
+import Icon from '@mdi/react';
+import { mdiMagnify } from '@mdi/js';
 
 function Autocomplete(props){
     const {name,cb} = props
     return(
-        <div onClick={cb}className="autocomplete">
+        <div onClick={cb} className="autocomplete">
+            <Icon className="autoGlasses" path={mdiMagnify} size={1} />
             {name}
         </div>
     )
