af8a6a12f03296e2fae2c4a5904a579585a38b90
feindreh <119032218+feindreh@users.noreply.github.com>
2023-04-07 03:18:53 +0200

    add background change based on time

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import "./style/reset.css"
 import "./style/App.css"
 
-import { useState,useEffect } from "react";
+import { useState,useEffect,useRef } from "react";
 
 import Content from "./components/content";
 
 import getWeather from "./API/Weather";
 
+import day from "./img/day.svg"
+import dawn from "./img/dawn.svg"
+import night from "./img/night.svg"
 
+import { getTime } from "./utility";
 
 function App() {
 
+
   const [data,setData] = useState({})
+  const [backGround,setBackGround] = useState(day)
+
+  function changeBackGround(time){
+    if(time <6 || time > 20){return night}
+    if(time < 16 && time > 8){return day}
+    return dawn
+  }
+
 
   useEffect(()=>{
    startSearch("Heidelberg")
@@ -24,10 +37,12 @@ function App() {
     //start the whole search api thing
     const data = await getWeather(cityName)
     setData(data)
+    const hour = +getTime(data.timezone).time.slice(0,2)
+    setBackGround(changeBackGround(hour))
   }
 
   return (
-    <div id="App">
+    <div style={{"backgroundImage" : `urL(${backGround})`}} id="App">
       <Content data={data} search={startSearch}/>
     </div>
   );
